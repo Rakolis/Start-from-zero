fbf45db9674aeb2046a4890cc8de006d8235d366
Rakolis <37342084+Rakolis@users.noreply.github.com>
2018-11-01 14:28:25 +0200

    Add files via upload

---
+++ table button img/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("demo").innerHTML = 8 + 8;
+function myFunction() {
+    document.getElementById("1").innerHTML = "Hello World";
+    document.getElementById("2").innerHTML = "pradingo";
+    document.getElementById("3").innerHTML = "ir cia tuscia";
+  }
+  function changeText(id) { 
+    id.innerHTML = "Ooops!";
+}
+function mOver(obj) {
+    obj.innerHTML = "Thank You"
+}
